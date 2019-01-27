a501c3e5511af5d0b016927992878225ec037a71
mrp149 <mikhailp@pacbell.net>
2019-01-26 23:20:23 -0800

    remove dead code

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"errors"
 	"flag"
 	"fmt"
 	"os"
@@ -12,62 +11,7 @@ import (
 var debug bool = false
 
 // by mikhailp@acm.org
-// processing options and flags
-
-// Example 3: A user-defined flag type, a slice of durations.
-type interval []time.Duration
-
-// String is the method to format the flag's value, part of the flag.Value interface.
-// The String method's output will be used in diagnostics.
-func (i *interval) String() string {
-	return fmt.Sprint(i)
-}
-
-// Set is the method to set the flag value, part of the flag.Value interface.
-// Set's argument is a string to be parsed to set the flag.
-// It's a comma-separated list, so we split it.
-func (i *interval) Set(value string) error {
-	// If we wanted to allow the flag to be set multiple times,
-	// accumulating values, we would delete this if statement.
-	// That would permit usages such as
-	//	-deltaT 10s -deltaT 15s
-	// and other combinations.
-	if len(*i) > 0 {
-		return errors.New("interval flag already set")
-	}
-	for _, dt := range strings.Split(value, ",") {
-		duration, err := time.ParseDuration(dt)
-		if err != nil {
-			return err
-		}
-		*i = append(*i, duration)
-	}
-	return nil
-}
-
-type inputDataFlags []string
-
-func (i *inputDataFlags) String() (s string) {
-	for pos, item := range *i {
-		s += fmt.Sprintf("Flag %d. %s\n", pos, item)
-	}
-	return
-}
-
-func (i *inputDataFlags) Set(value string) error {
-	*i = append(*i, value)
-	return nil
-}
-
-// Define a flag to accumulate durations. Because it has a special type,
-// we need to use the Var function and therefore create the flag during
-// init.
-
-var intervalFlag interval
-
-func init() {
-	//		flag.Var(&intervalFlag, "-deltaT", "comma-separated list of intervals to use between events")
-}
+//
 
 type Params struct {
 	act        bool
